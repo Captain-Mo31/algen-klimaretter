8a020a79985df3a2273284e4b105f3b99422a5e1
Captain-Mo31 <65597620+Captain-Mo31@users.noreply.github.com>
2022-06-07 11:04:28 +0200

    Allgemeine Verbesserungen

--- src/pages/anbau.svelte
+++ src/pages/anbau.svelte
@@ -6,8 +6,14 @@
   <p
     class="m-5 text-center text-jutify  w-11/12 md:w-3/4 mx-auto dark:text-white"
   >
-    Bei dem Algenanbau, unterscheidet man zwischen offenen und geschlossenen
-    Kultivierungssystemen. Beide haben ihren Zweck und ihre Vor- und Nachteile.
+    Algenfarmen produzieren jährlich mehr als 25 Millionen Tonnen. Der weltweite
+    Wert dieser Kultur übersteigt mit 6,4 Milliarden US-Dollar (Stand 2014)
+    knapp den von Zitronen. Die Algenzucht hat sich seit den späten 1950er
+    Jahren zu einem Wirtschaftszweig entwickelt, der nachhaltige Arbeitsplätze
+    in Entwicklungs- und Schwellenländern bietet.
+    <br /> <br />
+    Bei dem Algenanbau, unterscheidet man zwischen offenen und geschlossenen Kultivierungssystemen.
+    Beide haben ihren Zweck und ihre Vor- und Nachteile.
   </p>
   <p
     class="text-center text-2xl md:text-4xl dark:text-white font-apfel mt-3 w-11/12 md:w-3/4 mx-auto"
@@ -30,7 +36,7 @@
     dessen wenig Buget und Arbeitsaufwand, jedoch benötigen diese in der Regel
     eine große Fläche. Ein Beispiel für ein offenes System wäre ein Teller, Trog
     oder auch Tank am Land. Hinzu zu der Fläche, die diese Methode beanspruchen
-    würde kähme, das dieses System den Umwelteinflüssen ausgesetzt wäre und so
+    würde käme, dass dieses System den Umwelteinflüssen ausgesetzt wäre und so
     der Anbau recht unkontrolliert verlaufen würde. Das führt dazu, dass die
     Verwertungskette nicht ordnungsgemäß und optimal funktionieren kann, was
     wiederum dazu führt, das der eigentliche Zweck der Algen nicht erfüllt
@@ -39,7 +45,7 @@
   <p
     class="text-center text-2xl md:text-4xl dark:text-white font-apfel  w-11/12 md:w-3/4 mx-auto"
   >
-    Geschllossene Systeme
+    Geschlossene Systeme
   </p>
   <img
     alt="Group photo "
@@ -53,18 +59,15 @@
     geschlossenen Systeme dar.
     <br />
     <br />
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-    et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-    sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-    magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-    dolor sit amet.
+    Diese enthalten momentan überwiegend Mikroalgen und tretten zumeist in Form von
+    Photobioreaktoren auf, welche im Vergleich zu den offenen Systemen um einigeres
+    platzsparender sind. Diese haben den Vorteil, das ein Anbau um einiges effektiver
+    und gesteuerter Verlaufen kann, sodass die Algen unter optimalen Bedingungen
+    wachsen können. Hierraus ergibt sich eine schlussendliche hohe Qualtität an Algenprodukte,
+    aber auch ein ziemlich hoher Kosten-Nutzenpunkt, da diese Systeme einen hohen
+    Einsatz an Mitteln und Arbeitskräften erfordern. Momentan erfolgt die Gewinnung
+    von Algen **fast** an der Küste oder an küstennahen Meeresgebieten. Es gibt aber
+    auch schon Ansätzte Algen außerhalb dieses beschränkten Gebietes anzubauen.
   </p>
   <p
     class="text-center text-2xl md:text-4xl dark:text-white font-apfel  w-11/12 md:w-3/4 mx-auto"
@@ -78,19 +81,9 @@
     width="640"
     height="277"
   />
-  <p class="m-5 text-center  w-11/12 md:w-3/4 mx-auto dark:text-white">
+  <p class="pb-5 m-5 text-center  w-11/12 md:w-3/4 mx-auto dark:text-white">
     Momentan erfolgt die Gewinnung von Algen fast ausschließlich an der Küste
     oder an küstennahen Meeresgebieten. Es gibt aber auch schon Ansätzte Algen
     außerhalb dieses beschränkten Gebietes anzubauen.
-  </p>
-  <p class="m-5 text-center pb-5  w-11/12 md:w-3/4 mx-auto dark:text-white">
-    Algenfarmen produzieren jährlich mehr als 25 Millionen Tonnen. Der weltweite
-    Wert dieser Kultur übersteigt mit 6,4 Milliarden US-Dollar (2014) den von
-    Zitronen und Limetten. Die Algenzucht hat sich seit den späten 1950er Jahren
-    zu einem Wirtschaftszweig entwickelt, der nachhaltige Arbeitsplätze in
-    Entwicklungs- und Schwellenländern bietet, insbesondere in China (das mehr
-    als die Hälfte der weltweiten Gesamtmenge an Algen produziert - 12,8
-    Millionen Tonnen) und Indonesien (27 % der weltweiten Produktion - 6,5
-    Millionen Tonnen)
   </p>
 </div>
--- src/pages/probleme.svelte
+++ src/pages/probleme.svelte
@@ -4,5 +4,93 @@
   >
     Die Probleme mit den Algen
   </p>
-  <div class="w-full grid justify-items-center " />
+  <div class="w-full  p-5">
+    <div class="header flex items-end justify-between mb-12" />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div
+        class="overflow-hidden shadow-lg rounded-lg h-90 w-full md:11/12 cursor-pointer m-auto"
+      >
+        <a href="/stoffe" class="w-full block h-full">
+          <img
+            alt="Bild von schöner Natur"
+            src="/pictures/nachteile.webp"
+            class="max-h-40 w-full object-cover"
+            width="640"
+            height="489"
+          />
+          <div class=" dark:bg-gray-800 w-full p-4">
+            <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+              Gefährliche Stoffe in den Algen
+            </p>
+            <p class="text-gray-400 dark:text-gray-200 font-light text-md">
+              Welche gefärlichen Stoffen können sich in Algen befinden
+            </p>
+          </div>
+        </a>
+      </div>
+      <div
+        class="overflow-hidden shadow-lg rounded-lg h-90 w-full md:11/12 cursor-pointer m-auto"
+      >
+        <a href="/anbau" class="w-full block h-full">
+          <img
+            alt="Bild von Algen auf dem Wasser"
+            src="/pictures/.webp"
+            class="max-h-40 w-full object-cover"
+            width="640"
+            height="427"
+          />
+          <div class=" dark:bg-gray-800 w-full p-4">
+            <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+              Anbau
+            </p>
+            <p class="text-gray-400 dark:text-gray-200 font-light text-md">
+              Wie lassen sich Algen nachhaltig anbauen?
+            </p>
+          </div>
+        </a>
+      </div>
+      <div
+        class="overflow-hidden shadow-lg rounded-lg h-90 w-full md:11/12 cursor-pointer m-auto"
+      >
+        <a href="/nutzung" class="w-full block h-full">
+          <img
+            alt="Kleine Plastik Teilchen"
+            src="/pictures/.webp"
+            class="max-h-40 w-full object-cover"
+            width="640"
+            height="480"
+          />
+          <div class=" dark:bg-gray-800 w-full p-4">
+            <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+              Nutzung
+            </p>
+            <p class="text-gray-400 dark:text-gray-200 font-light text-md">
+              Welche Nutzungsmöglichkeiten bieten Algen?
+            </p>
+          </div>
+        </a>
+      </div>
+      <div
+        class="overflow-hidden shadow-lg rounded-lg h-90 w-full md:11/12 cursor-pointer m-auto"
+      >
+        <a href="/probleme" class="w-full block h-full">
+          <img
+            alt="Puzel"
+            src="/pictures/.webp"
+            class="max-h-40 w-full object-cover"
+            width="640"
+            height="427"
+          />
+          <div class=" dark:bg-gray-800 w-full p-4">
+            <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+              Probleme
+            </p>
+            <p class="text-gray-400 dark:text-gray-200 font-light text-md">
+              Welche Probleme gibt es noch?
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
