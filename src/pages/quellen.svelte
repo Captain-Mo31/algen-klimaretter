880ba29fcef98530116019895c78e33c5288eee2
Captain-Mo31 <65597620+Captain-Mo31@users.noreply.github.com>
2022-06-12 10:32:21 +0200

    Update quellen.svelte

--- src/pages/quellen.svelte
+++ src/pages/quellen.svelte
@@ -3,37 +3,37 @@
     Quellen:
   </p>
 
-  <p class="py-2 text-black dark:text-white break-all" />
+  <p class="py-2 text-black dark:text-white break-all md:break-words" />
   <p class="text-center  text-2xl md:text-4xl pb-5 font-apfel dark:text-white">
     Bilder:
   </p>
   <ul class="list-disc">
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Apple Memoji. Apple Memoji wurde verwendet um die Avatare auf der
       Startseite zu erstellen.
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Gutsverwaltung Schönfelder Hochland o. J.: Biogasanlage. Internet:
       https://www.gutsverwaltung-dresden.de/biogasanlage/ (10.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Johannes Kopton (22.08.2019): Superfood von der Fensterbank. Internet:
       https://progressive-agrarwende.org/superfood-von-der-fensterbank/
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       MySafetySign o. J.: Hazardous Materials Signs. Internet:
       https://www.mysafetysign.com/hazardous-materials-signs (10.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Publicdomainvectors (2017) Einfache Darstellung der fossilen Pflanze.
       Internet:
@@ -41,109 +41,109 @@
       (10.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Venture Magazine o. J.: Could your car or airplane soon run on this slimy,
       widespread menace? Internet:
       https://www.theventuremag.com/algae-biodiesel/ (10.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/NfkWUOkB78c
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/aYPtEknQmXE
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/8mikJ83LmSQ
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/yvn5UBRamAM
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/4MEL9XS-3JQ
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/LEcX987KByw
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/FxnqdmKBJps
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/W3y2crFkVIs
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/RVX2STx44UI
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/b8vCfi9UsTU
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/jX6WXNkvsPs
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/U2lJNhwUYAE
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/p4q2n4qFraY
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/7uKj8RqNiIs
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/PuwfRakCJvk
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/SRFOOdORhKw
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/23WUGwMsFH8
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/UNs7kXHGIe0
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/9aAr_EFqYdc
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       https://unsplash.com/photos/pmfJcN7RGiw
     </li>
@@ -156,7 +156,7 @@
       Sonstige-Nutzung:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Martina Hilz 2019: Algenwälder als Hoffnungsträger. Internet:
       https://enorm-magazin.de/umwelt/umweltschutz/klimaschutz/seegraswiesen-und-algenwalder-als-hoffnungstraeger
@@ -166,27 +166,27 @@
       Probleme:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Martina Hilz 2019: Algenwälder als Hoffnungsträger. Internet:
       https://enorm-magazin.de/umwelt/umweltschutz/klimaschutz/seegraswiesen-und-algenwalder-als-hoffnungstraeger
       (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Decker 2018: Algen als Treibstoff. Internet:
       https://www.planet-wissen.de/natur/pflanzen/algen/pwiealgenimtank100.html
       (03.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Decker 2018: Algen. Internet:
       https://www.planet-wissen.de/natur/pflanzen/algen/index.html (29.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Svjetlana Pejic-Pulkowski o. J.: Was Sie über Algen wissen sollten.
       Internet:
@@ -197,40 +197,40 @@
       Nutzung als Energie:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Anni Roenkae (2021): Wasserstoff aus Algen. Internet:
       https://www.energieleben.at/wasserstoff-aus-algen/ (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Kb (2008): Wasserstoffproduktion mit Algen. Internet:
       https://www.biooekonomie-bw.de/fachbeitrag/pm/wasserstoffproduktion-mit-algen
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Moritz Eick (2013): Klimawandel: Lösungsansatz Alge. Internet:
       https://prezi.com/sptd3ackzpre/klimawandel-losungsansatz-alge/
       (03.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Peter Carstens o. J.: Biogas aus der Algenfabrik. Internet:
       https://www.geo.de/natur/nachhaltigkeit/3806-rtkl-erneuerbare-energien-biogas-aus-der-algenfabrik
       (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Decker 2018: Algen als Treibstoff. Internet:
       https://www.planet-wissen.de/natur/pflanzen/algen/pwiealgenimtank100.html
       (03.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Decker 2018: Algen. Internet:
       https://www.planet-wissen.de/natur/pflanzen/algen/index.html (29.05.2022)
@@ -239,14 +239,14 @@
       Lunge:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Constanze Alvarez (2021): Die grüne Lunge der Meere. Internet:
       https://www.br.de/wissen/seegras-seegraswiesen-posidonia-meer-kohlenstoff-klimawandel-100.html
       (10.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Helmholtz Klima Initiative o. J.: "Für Seegraswiesen könnten die
       Auswirkungen schwerwiegend sein". Internet:
@@ -254,21 +254,21 @@
       (13.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Martina Hilz 2019: Algenwälder als Hoffnungsträger. Internet:
       https://enorm-magazin.de/umwelt/umweltschutz/klimaschutz/seegraswiesen-und-algenwalder-als-hoffnungstraeger
       (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Kraus (2017): Mit Algen gegen Luftverschmutzung. Internet:
       https://www.deutschlandfunk.de/filteranlage-in-paris-mit-algen-gegen-luftverschmutzung-100.html
       (25.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Svjetlana Pejic-Pulkowski o. J.: Was Sie über Algen wissen sollten.
       Internet:
@@ -276,7 +276,7 @@
       (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Vanessa Reske (2019): Darum sollten wir Algen lieben. Internet:
       https://www.quarks.de/umwelt/klimawandel/darum-sollten-wir-algen-lieben/
@@ -286,13 +286,13 @@
       Leber:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Beke Enderstein(2021): Algen essen: Gesund oder gefährlich? Internet:
       https://www.sevencooks.com/de/magazin/algen-essen-gesund-oder-gefaehrlich-6SnYArpMaaahFz8mfCW32t
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Vanessa Reske (2019): Darum sollten wir Algen lieben. Internet:
       https://www.quarks.de/umwelt/klimawandel/darum-sollten-wir-algen-lieben/
@@ -302,14 +302,14 @@
       Anbau:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       AlgenProjekt o. J.: Algenkultivierung im Meer. Internet:
       https://www.algenprojekt.de/algenkultivierung/algenkultivierung-im-meer/
       (20.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Emma-Jane Cook, Nidhi Nagabhatla, Yacine Badis und weitere: Safeguarding
       the future of the global seaweed aquaculture industry. Internet:
@@ -317,17 +317,17 @@
       (03.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Martina Hilz 2019: Algenwälder als Hoffnungsträger. Internet:
       https://enorm-magazin.de/umwelt/umweltschutz/klimaschutz/seegraswiesen-und-algenwalder-als-hoffnungstraeger
       (07.06.2022)
     </li>
     <p class="text-center  text-xl md:text-3xl pb-5 font-apfel dark:text-white">
-      Allgemein:
+      all md:break-wordsgemein:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Vanessa Reske (2019): Darum sollten wir Algen lieben. Internet:
       https://www.quarks.de/umwelt/klimawandel/darum-sollten-wir-algen-lieben/
@@ -338,14 +338,14 @@
       Nutzung als Nahrung:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Peter Hermes (2018): Grüne Welle. Internet:
       https://www.faz.net/aktuell/stil/essen-trinken/kann-die-weltbevoelkerung-durch-meeresalgen-ernaehrt-werden-15909885.html
       (13.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Susanne Decker 2018: Algen. Internet:
       https://www.planet-wissen.de/natur/pflanzen/algen/index.html (29.05.2022)
@@ -354,12 +354,12 @@
       Nutzung als Plastik:
     </p>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Bloom o.J.: Bloom. Internet: https://www.bloommaterials.com/ (29.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Jonas Buske (2020): Algen als Plastikersatz: Löst das unser Umweltproblem?
       Internet:
@@ -367,14 +367,15 @@
       (07.06.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
-      Kle o. J.: Alge statt Plastik - Der natürliche Alleskönner. Internet:
+      Kle o. J.: Alge statt Plastik - Der natürliche all md:break-wordseskönner.
+      Internet:
       https://www.ecowoman.de/koerper-geist/yoga/schaumstoff-und-plastikstoffe-aus-algenblueten-als-nachhaltiger-eco-ersatz-4245
       (29.05.2022)
     </li>
     <li
-      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all"
+      class="text-jutify  w-11/12 md:w-3/4  mx-auto dark:text-white break-all md:break-words"
     >
       Plastikalternative o. J.: Können Algen eine Alternative zu Kunststoffen
       sein? Internet: https://www.plastikalternative.de/verpackungen-aus-algen/
